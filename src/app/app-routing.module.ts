efe9c46bff123611ab19908eb4c92fbe20682e0d
ivor pixabaj <pixabaji@gmail.com>
2020-11-17 12:58:54 -0600

    Initiate app-Artment project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from '../app/landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home-page', pathMatch: 'full'},
+  {path: 'home-page', component: LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
