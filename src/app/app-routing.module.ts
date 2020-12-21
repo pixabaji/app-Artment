e1d29bd0dc1354feee8ca405efe0bf7b0fb321bb
ivor pixabaj <pixabaji@gmail.com>
2020-12-20 23:57:38 -0600

    feat (apertment) - added apartment listing shell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from '../app/landing-page/landing-page.component';
+import { ApartmentListComponent } from './apartment-list/apartment-list.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'home-page', pathMatch: 'full'},
-  {path: 'home-page', component: LandingPageComponent}
+  {path: 'home-page', component: LandingPageComponent},
+  {path: 'apts-list', component: ApartmentListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingHeaderComponent } from './landing-page/landing-header/landing-header.component';
+import { ApartmentListComponent } from './apartment-list/apartment-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    LandingHeaderComponent,
+    ApartmentListComponent
   ],
   imports: [
     BrowserModule,
